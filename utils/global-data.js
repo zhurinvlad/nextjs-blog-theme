cb5ddc4f9192bd258c869438e6494ba0ce345c78
Vladislav Zhurin <vlad032_grax@mail.ru>
2023-12-20 21:20:18 +0400

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Лицей №2';
+    : 'МБОУ «Брянский городской лицей №2 им.М.В. Ломоносова»';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Наш родной язык';
